a86ef247d120cfc0ec04a06601b758f3cff7ed4d
Craig Ziesman <cziesman@redhat.com>
2026-09-02 17:45:10 -0500

    debugging

--- src/main/java/com/redhat/autoshift/report/controller/DashboardController.java
+++ src/main/java/com/redhat/autoshift/report/controller/DashboardController.java
@@ -2,6 +2,8 @@ package com.redhat.autoshift.report.controller;
 
 import com.redhat.autoshift.report.model.Report;
 import com.redhat.autoshift.report.service.PolicyReportService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DashboardController.class);
     @Autowired
     private PolicyReportService service;
 
@@ -17,6 +20,11 @@ public class DashboardController {
     public String index(Model model) throws Exception {
 
         Report report = service.report();
+        LOG.info(
+                "GET / - report loaded: {} policies, {} clustersets, {} clusters",
+                report.policies().size(),
+                report.clusterSets().size(),
+                report.clusters().size());
         model.addAttribute("report", report);
         model.addAttribute("currentPage", "summary");
         model.addAttribute("policiesRepository", service.policiesRepositoryInfo());
